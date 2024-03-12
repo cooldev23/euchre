2bd61088ee55c0f36d4860e4e698f77e69681260
src_code_dev <scordill23@gmail.com>
2024-03-11 21:31:51 -0600

    Start fleshing out playing view

---
+++ src/components/CardPlayer.vue
@@ -0,0 +1,24 @@
+<template>
+  <h5>{{ player.name }}</h5>
+  <div class="cards">
+    <PlayingCard
+      v-for="(card, index) in player.cards"
+      :key="index"
+      :card-value="card"
+    />
+  </div>
+</template>
+
+<script setup>
+  import PlayingCard from './PlayingCard.vue';
+
+  // props
+  const props = defineProps({
+    player: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }
+  })
+</script>
--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -1,14 +1,63 @@
 <script setup>
-import TestCard from '@/components/icons/TestCard.vue';
+import CardPlayer from '@/components/CardPlayer.vue';
 import BaseView from './BaseView.vue';
+import { ref, onBeforeMount } from 'vue'
 
+// data
+const players = ref([
+  {
+    name: 'Player 1', 
+    cards: []
+  },
+  {
+    name: 'Player 2', 
+    cards: []
+  },
+  {
+    name: 'Player 3', 
+    cards: []
+  },
+  {
+    name: 'Player 4', 
+    cards: []
+  }
+])
 
+const possibleCards = ref([
+  '9H', '9S', '9D', '9C',
+  'TH', 'TS', 'TD', 'TC',
+  'JH', 'JS', 'JD', 'JC',
+  'QH', 'QS', 'QD', 'QC',
+  'KH', 'KS', 'KD', 'KC',
+  'AH', 'AS', 'AD', 'AC'
+]);
+
+function dealCards() {
+  for (let i = 0; i < possibleCards.value.length; i++) {
+    let index = Math.floor(Math.random() * possibleCards.value.length),
+    card = possibleCards.value.slice(index, index+1),
+    player = players.value[Math.floor(Math.random() * players.value.length)];
+    player.cards.push(card[0]);
+  }
+  
+  
+}
+
+onBeforeMount(() => {
+  dealCards()
+})
 </script>
 
 <template>
   <BaseView>
     <template #content>
-      <TestCard/>
+      <div class="h-1/3">
+        <CardPlayer
+          v-for="(player, index) in players"
+          :key="index"
+          :player="player"
+        />
+      </div>
     </template>
   </BaseView>
 </template>
